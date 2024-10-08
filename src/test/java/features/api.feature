22df680c65328666b94cf35d345d0c3d990f16b7
nnnuram <nnnuram22@yahoo.com>
2024-10-08 17:27:06 +0700

    Update1

---
+++ src/test/java/features/api.feature
@@ -0,0 +1,4 @@
+Feature: Test Automation Rest Api
+
+  Scenario: Test get list data normal
+    Given  Prepare url valid for get list data
---
+++ src/test/java/stepDef/ApiStep.java
@@ -0,0 +1,10 @@
+package stepDef;
+
+import io.cucumber.java.en.Given;
+
+public class ApiStep {
+    @Given("Prepare url valid for get list data")
+    public void prepareUrlValidForGetListData() {
+        System.out.println("Hello Step");
+    }
+}
